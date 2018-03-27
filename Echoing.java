048f2f61407df8e0cff1ae872c78137061bc3a7d
Ronak Agrawal <ronakagrawal04@gmail.com>
2018-03-27 11:13:03 -0400

    Create Echoing.java

---
+++ Echoing.java
@@ -0,0 +1,10 @@
+// This is just to give a glinch of command line argument
+// Just give some variable as argument during command line and print using basic io methods.
+
+public class Echoing {
+    public static void main (String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println(args[i]);
+    }
+}
+ 
